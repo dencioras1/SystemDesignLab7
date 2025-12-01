437cfbff8109fcd1dec9dbe1c7f9a08921ea7c26
luisA <luisadc9898@gmail.com>
2025-12-01 16:56:23 +0100

    Added TryClass.java to branch

---
+++ src/TryClass.java
@@ -0,0 +1,9 @@
+public class TryClass {
+
+    public void talk(){
+        System.out.println("I'm trying to talk");
+    }
+    public void talk2(){
+        System.out.println("I'm trying to talk2");
+    }
+}
