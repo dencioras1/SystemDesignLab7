fd156b3f0ba39b07abdc42ae71a28426e95609f7
luisA <luisadc9898@gmail.com>
2025-12-01 16:31:56 +0100

    Added Addition Method to Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,16 @@
 public class Hello {
 
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        System.out.println(add(1,2));
+
     }
 
 }
