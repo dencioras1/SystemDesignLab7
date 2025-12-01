ff166d09dc4efaf56c2daf540074b161ee9e799f
luisA <luisadc9898@gmail.com>
2025-12-01 16:34:18 +0100

    Added epicMethod() to main

--- src/Hello.java
+++ src/Hello.java
@@ -9,6 +9,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
+        epicMethod();
+
         System.out.println(add(1,2));
 
     }
